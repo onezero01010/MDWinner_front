930243b977ec374a0557776872a88708fa286ea1
송원영 <rachelsong10@snu.ac.kr>
2025-06-26 11:58:29 +0900

    chore: React + typescript + tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
+      <p className="mt-4 text-gray-700">TailwindCSS가 잘 작동하고 있어요 😎</p>
+    </div>
+  )
+}
+
+export default App
